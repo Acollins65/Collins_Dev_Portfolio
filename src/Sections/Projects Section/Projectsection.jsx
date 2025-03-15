2dbd9561921b4f57448a8a9f6d7becf9fe8cbf6c
Alfred Collins-Bimpong <bimpong25@outlook.com>
2025-03-15 15:29:00 +0000

    Project Section Initialise

---
+++ src/Sections/Projects Section/Projectsection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div>
+      <h1 style={{ color: "red", fontSize: "90px" }}>Project section</h1>
+    </div>
+  );
+};
+
+export default Projects;
